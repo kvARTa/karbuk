84ae4888fb733e09f41f12bef35c0aa788da28ee
22rostislav <22rostislav@gmail.com>
2014-05-27 15:38:30 +0300

    cache manager

---
+++ admin/language/russian/catalog/cachemanager.php
@@ -0,0 +1,21 @@
+<?php
+// Heading
+$_['heading_title']     = 'Кэш';
+
+// Text
+$_['text_success']      = 'Кэш удален!';
+$_['text_default']      = 'По умолчанию';
+
+// Column
+$_['column_name']       = 'Имя файла кэша';
+$_['column_sort_order'] = 'Порядок сортировки';
+$_['column_expire']     = 'Срок действия (в минутах)';
+$_['column_action']     = 'Действие';
+
+// Entry
+$_['entry_name']        = 'Имя кэша:';
+$_['entry_sort_order']  = 'Порядок сортировки:';
+
+// Error 
+$_['error_permission']  = 'У Вас нет прав для управления кэшем!';
+?>
